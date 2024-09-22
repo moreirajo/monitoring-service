492b5a55a8000b8fc33b4652897592f7c8fd594f
moreirajo4 <joaof4moreira@gmail.com>
2024-09-22 15:53:39 +0100

    fix concurrency issue

--- src/main/java/com/hansecom/monitoringservice/service/impl/JobServiceImpl.java
+++ src/main/java/com/hansecom/monitoringservice/service/impl/JobServiceImpl.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.Optional;
 import java.util.TimeZone;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.quartz.CronScheduleBuilder;
@@ -16,6 +18,7 @@ import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +29,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class JobServiceImpl implements JobService {
 
+  private final Lock jobValidationLock = new ReentrantLock();
+
   private final Scheduler scheduler;
 
   @Value("${custom-properties.max-jobs-allowed:5}")
@@ -35,13 +40,7 @@ public class JobServiceImpl implements JobService {
   @Override
   public JobDto createJob(@NotNull @Valid final JobDto jobDto) {
 
-    if (scheduler.getJobKeys(GroupMatcher.anyGroup()).size() >= maxJobsAllowed) {
-      throw StandardException.maxJobsAllowed(maxJobsAllowed);
-    }
-
-    if (scheduler.checkExists(JobKey.jobKey(jobDto.getName()))) {
-      throw StandardException.jobNameAlreadyExists(jobDto.getName());
-    }
+    validations(jobDto);
 
     JobDetail jobDetail =
         JobBuilder.newJob(UrlMonitorJob.class)
@@ -64,5 +63,34 @@ public class JobServiceImpl implements JobService {
     scheduler.scheduleJob(jobDetail, trigger);
 
     return jobDto;
+  }
+
+  /**
+   * Performs validations on the job before it is scheduled. This method checks if the maximum
+   * number of allowed jobs has been reached and if a job with the given name already exists.
+   *
+   * <p>This method is thread-safe. It uses a ReentrantLock to ensure that only one thread can
+   * perform validations at a time. This prevents race conditions when multiple threads attempt to
+   * create jobs simultaneously, maintaining the integrity of the job count and uniqueness of job
+   * names.
+   *
+   * @param jobDto the JobDto object containing the details of the job to be validated
+   * @throws SchedulerException if there's an error while interacting with the scheduler.
+   * @throws StandardException if the maximum number of allowed jobs has been reached or if a job
+   *     with the given name already exists
+   */
+  private void validations(JobDto jobDto) throws SchedulerException {
+    jobValidationLock.lock();
+    try {
+      if (scheduler.getJobKeys(GroupMatcher.anyGroup()).size() >= maxJobsAllowed) {
+        throw StandardException.maxJobsAllowed(maxJobsAllowed);
+      }
+
+      if (scheduler.checkExists(JobKey.jobKey(jobDto.getName()))) {
+        throw StandardException.jobNameAlreadyExists(jobDto.getName());
+      }
+    } finally {
+      jobValidationLock.unlock();
+    }
   }
 }
